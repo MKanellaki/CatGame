6cab68dd6e5f5641b0d748fed37b37ef13923de6
MKanellaki <manellaki15@gmail.com>
2024-12-20 21:29:27 +0200

    add engine

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <SDL2/SDL_image.h>
+#include "engine/game.h"
+#include "engine/common.h"
+#include "engine/sprite.h"
+#include "engine/interface.h"
+#include "engine/timers.h"
+#include "engine/audio.h"
+
+#define WIDTH 640
+#define HEIGHT 640
+
+int main(){
+    
+}
