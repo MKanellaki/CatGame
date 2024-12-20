c62dd0c712f15bc0264362cdc92e36571e77917b
MKanellaki <manellaki15@gmail.com>
2024-12-20 23:56:36 +0200

    add animations

--- src/main.c
+++ src/main.c
@@ -17,58 +17,71 @@ static struct
     // A collection of assets used by entities
     // Ideally, they should have been automatically loaded
     // by iterating over the res/ folder and filling in a hastable
-    SDL_Texture *player_texture, *explosion_texture;
+    SDL_Texture *run_texture, *jump_texture, *idle_texture, *sleep_texture;
     Mix_Chunk *gem_sfx;
     TTF_Font *main_font;
 
     ng_sprite_t player;
     ng_label_t welcome_text;
-    ng_animated_sprite_t explosion;
+    ng_animated_sprite_t run, jump, idle, sleep;
 } ctx;
 
 static void create_actors(void)
 {
-    ng_game_create(&ctx.game, "Hello World!", WIDTH, HEIGHT);
+    ng_game_create(&ctx.game, "Cat", WIDTH, HEIGHT); //creates window
 
-    ctx.main_font = TTF_OpenFont("res/free_mono.ttf", 16);
-    ctx.player_texture = IMG_LoadTexture(ctx.game.renderer, "res/player.png");
-    ctx.explosion_texture = IMG_LoadTexture(ctx.game.renderer, "res/explosion.png");
-    ctx.gem_sfx = ng_audio_load("res/gem.wav");
+ 
+    //We load the animations (run, jump, idle, sleep)
+    ctx.run_texture = IMG_LoadTexture(ctx.game.renderer, "assets/cat/run.png");
+    ctx.jump_texture = IMG_LoadTexture(ctx.game.renderer, "assets/cat/jump.png");
+    ctx.idle_texture = IMG_LoadTexture(ctx.game.renderer, "assets/cat/idle.png");
+    ctx.sleep_texture = IMG_LoadTexture(ctx.game.renderer, "assets/cat/sleep.png");
 
     ng_interval_create(&ctx.game_tick, 50);
-    ng_sprite_create(&ctx.player, ctx.player_texture);
-    ng_sprite_set_scale(&ctx.player, 2.0f);
-    ctx.player.transform.y = HEIGHT - ctx.player.transform.h - 10;
+    
+    //Create animations (run, jump, idle, sleep)
+    ng_animated_create(&ctx.run, ctx.run_texture, 7);  //run
+    ng_sprite_set_scale(&ctx.run.sprite, 2.0f);
+    ctx.run.sprite.transform.x = 200.0f;
+    
+    ng_animated_create(&ctx.jump, ctx.jump_texture, 13);  //jump
+    ng_sprite_set_scale(&ctx.jump.sprite, 2.0f);
+    ctx.jump.sprite.transform.x = 200.0f;
+    
+    ng_animated_create(&ctx.idle, ctx.idle_texture, 9);  //idle
+    ng_sprite_set_scale(&ctx.idle.sprite, 2.0f);
+    ctx.idle.sprite.transform.x = 200.0f;
+    
+    ng_animated_create(&ctx.sleep, ctx.sleep_texture, 9);  //sleep
+    ng_sprite_set_scale(&ctx.sleep.sprite, 2.0f);
+    ctx.sleep.sprite.transform.x = 200.0f;
 
-    ng_animated_create(&ctx.explosion, ctx.explosion_texture, 9);
-    ng_sprite_set_scale(&ctx.explosion.sprite, 2.0f);
-    ctx.explosion.sprite.transform.x = 200.0f;
 
-    ng_label_create(&ctx.welcome_text, ctx.main_font, 300);
-    ng_label_set_content(&ctx.welcome_text, ctx.game.renderer,
-            "I don't really know what to write in here. "
-            "You'll notice that you can't type Greek. To do that, "
-            "just replace the internals with TTF_RenderUTF8_Solid");
 }
 
-// A place to handle queued events.
 static void handle_event(SDL_Event *event)
 {
     switch (event->type)
     {
     case SDL_KEYDOWN:
-        // Press space for a sound effect!
-        if (event->key.keysym.sym == SDLK_SPACE)
-            ng_audio_play(ctx.gem_sfx);
+        if (event->key.keysym.sym == SDLK_SPACE){
+            
+            if (ng_interval_is_ready(&ctx.game_tick))
+            {
+                ng_animated_set_frame(&ctx.jump, (ctx.jump.frame + 1)
+                % ctx.jump.total_frames);
+            }
+            
+        }
 
         break;
 
-    case SDL_MOUSEMOTION:
-        // Move label on mouse position
-        // By the way, that's how you can implement a custom cursor
-        ctx.welcome_text.sprite.transform.x = event->motion.x;
-        ctx.welcome_text.sprite.transform.y = event->motion.y;
-        break;
+    // case SDL_MOUSEMOTION:
+    //     // Move label on mouse position
+    //     // By the way, that's how you can implement a custom cursor
+    //     ctx.welcome_text.sprite.transform.x = event->motion.x;
+    //     ctx.welcome_text.sprite.transform.y = event->motion.y;
+    //     break;
     }
 }
 
@@ -80,15 +93,15 @@ static void update_and_render_scene(float delta)
     if (keys[SDL_SCANCODE_LEFT]) ctx.player.transform.x -= 640* delta;
     if (keys[SDL_SCANCODE_RIGHT]) ctx.player.transform.x += 640* delta;
 
-    // Update the explosion's frame once every 100ms
-    if (ng_interval_is_ready(&ctx.game_tick))
-    {
-        ng_animated_set_frame(&ctx.explosion, (ctx.explosion.frame + 1)
-                % ctx.explosion.total_frames);
-    }
+    //Update the explosion's frame once every 100ms
+    // if (ng_interval_is_ready(&ctx.game_tick))
+    // {
+    //     ng_animated_set_frame(&ctx.jump, (ctx.jump.frame + 1)
+    //             % ctx.jump.total_frames);
+    // }
 
     ng_sprite_render(&ctx.player, ctx.game.renderer);
-    ng_sprite_render(&ctx.explosion.sprite, ctx.game.renderer);
+    ng_sprite_render(&ctx.jump.sprite, ctx.game.renderer);
     ng_sprite_render(&ctx.welcome_text.sprite, ctx.game.renderer);
 }
 
